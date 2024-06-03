10441875f86a5b8ff0c3cfa1c5029fd8c736b1f5
Ttunay <tigrus232323k@gmail.com>
2024-06-03 16:57:48 +0300

    write main.c cli.c and cli.h

--- src/main.c
+++ src/main.c
@@ -1,15 +1,29 @@
+#include <stdio.h>
 #include "cli.h"
-#include "package_manager.h"
 
 int main(int argc, char *argv[]) {
-    // Инициализация системы
-    initialize();
+ //   initialize();
+    // cleanup();
+        parse_command(argc, argv);
 
-    // Парсинг команд
-    parse_cmd(argc, argv);
+}
 
-    // Очистка ресурсов перед завершением
-    cleanup();
 
-    return 0;
+void parse_command(int argc, char *argv[]) {
+
+    printf("argc будет равно %d, потому что есть %d аргумента: ", argc, argc);
+    for (int i = 0; i < argc; i++) {
+        printf("%s", argv[i]);
+        if (i < argc - 1) {
+            printf(", ");
+        }
+    }
+    printf(".\n");
+
+    printf("argv будет массивом строк:\n");
+    for (int i = 0; i < argc; i++) {
+        printf("    argv[%d] = \"%s\"\n", i, argv[i]);
+    }
+    printf("    argv[%d] = NULL (указывает на конец массива)\n", argc);
+
 }
