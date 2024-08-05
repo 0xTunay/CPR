91d2c0bbf240dd7faf00a45613cf03d6ce1e6836
Ttunay <tigrus232323k@gmail.com>
2024-08-05 15:39:16 +0300

    add infynity cycle

--- src/main.c
+++ src/main.c
@@ -5,72 +5,58 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    load_repository();
+  load_repository();
 
-    char buffer[100];
-    char pkg_name[MAX_NAME_LENGTH];
-    char *command;
-    char d[] = " ";
+  char buffer[100];
+  char pkg_name[MAX_NAME_LENGTH];
+  char *command;
+  char space[] = " ";
 
+  while (1) {
     if (argc < 2) {
-        printf("Write cmd:\n");
-        fgets(buffer, sizeof(buffer), stdin);
+      puts("Write cmd:");
+      if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        perror("fgets failed");
+        return 1;
+      }
 
-        int leng = strlen(buffer);
-        if (buffer[leng - 1] == '\n') {
-            buffer[leng - 1] = '\0';
-        }
+      int leng = strlen(buffer);
+      if (buffer[leng - 1] == '\n') {
+        buffer[leng - 1] = '\0';
+      }
 
-        command = (char *)malloc((leng + 1) * sizeof(char));
-        if (command == NULL) {
-            perror("memory allocation");
-            return 1;
-        }
-        strcpy(command, buffer);
+      command = (char *)malloc((leng + 1) * sizeof(char));
+      if (command == NULL) {
+        perror("memory allocation");
+        return 1;
+      }
+      strcpy(command, buffer);
+      char *cmd1 = strtok(command, space);
+      char *cmd2 = strtok(NULL, space);
+      char *pkgname = strtok(NULL, space);
 
-        char *cmd1 = strtok(command, d);
-        char *cmd2 = strtok(NULL, d);
-        char *pkgname = strtok(NULL, d);
-
-        if (pkgname) {
-            strcpy(pkg_name, pkgname);
-            PkgInfo *pkg = get_pkg_info(pkg_name);
-            if (pkg) {
-                printf("Package found: %s\n", pkg->name);
-                printf("Package URL: %s\n", pkg->url);
-
-                char *argv_new[3];
-                argv_new[0] = cmd1;
-                argv_new[1] = cmd2;
-                argv_new[2] = pkgname;
-
-                int argc_new = 3;
-
-                parse_command(argc_new, argv_new);
-            } else {
-                printf("Package %s not found\n", pkg_name);
-            }
-        } else {
-            printf("No package name provided in the command.\n");
-            free(command);
-            return 0;
-        }
-
-        free(command);
-    } else {
-        strcpy(pkg_name, argv[2]);
-
+      if (pkgname) {
+        strcpy(pkg_name, pkgname);
         PkgInfo *pkg = get_pkg_info(pkg_name);
-
         if (pkg) {
-            printf("Package found: %s\n", pkg->name);
-            printf("Package URL: %s\n", pkg->url);
+          printf("Package found: %s\n", pkg->name);
+          printf("Package URL: %s\n", pkg->url);
 
-            parse_command(argc, argv);
+          char *argv_new[3] = {cmd1, cmd2, pkgname};
+          int argc_new = 3;
+          parse_command(argc_new, argv_new);
+          free(command);
+          break; 
         } else {
-            printf("Package %s not found\n", pkg_name);
+          printf("Package %s not found\n", pkg_name);
         }
-    }
+      } else {
+        printf("No package name provided in the command.\n");
+      }
 
-    return 0;
+      free(command); 
+    }
+  }
+
+  return 0;
 }
